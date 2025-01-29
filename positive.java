d7e6e43496c398385e120fd2fb2155c2a07c967c
vinod kumar <sirikondaviinod99@gmail.com>
2025-01-29 16:59:50 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,15 @@
+public class positive {
+    public static void main(String[] args) {
+        int a=-15;
+        if(a>0){
+            System.out.println("positive");
+        }
+        if(a==0){
+            System.out.println("zero");
+        }
+        if(a<0){
+            System.out.println("negative");
+        } 
+
+    }
+}
